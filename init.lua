cab59a47bc80f81e528e953e3a8edacd5586f3d6
Sr. Meia-noiteλ (LordZarkares) <lordzarkares@Outlook.com>
2022-11-08 02:34:02 -0300

    .

--- init.lua
+++ init.lua
@@ -1,17 +1,53 @@
 local opt = vim.opt ;
 local cmd = vim.cmd ;
 
-opt.number      = true  ;
-opt.wrap        = false ;
-opt.cursorline  = true  ;
-opt.tabstop     = 4     ;
-opt.softtabstop = 4     ;
-opt.shiftwidth  = 4     ;
-opt.expandtab   = true  ;
-opt.incsearch   = true  ;
-opt.hlsearch    = true  ;
+opt.number        = true  ;
+opt.wrap          = false ;
+opt.cursorline    = true  ;
+opt.tabstop       = 4     ;
+opt.softtabstop   = 4     ;
+opt.shiftwidth    = 4     ;
+opt.expandtab     = true  ;
+opt.incsearch     = true  ;
+opt.hlsearch      = true  ;
+opt.termguicolors = true  ;
 
-cmd [[ colorscheme gruvbox ]]
+require('nvim-treesitter.configs').setup {
+    ensure_installed = { "lua", "haskell", "python" },
+    sync_install = false,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+    },
+    indent = { enable = true }
+}
+
+require('onedarkpro').setup {
+    styles = { -- Choose from "bold,italic,underline"
+        types        = "bold", -- Style that is applied to types
+        numbers      = "NONE", -- Style that is applied to numbers
+        strings      = "NONE", -- Style that is applied to strings
+        comments     = "NONE", -- Style that is applied to comments
+        keywords     = "NONE", -- Style that is applied to keywords
+        constants    = "NONE", -- Style that is applied to constants
+        functions    = "NONE", -- Style that is applied to functions
+        operators    = "NONE", -- Style that is applied to operators
+        variables    = "NONE", -- Style that is applied to variables
+        conditionals = "NONE", -- Style that is applied to conditionals
+        virtual_text = "NONE", -- Style that is applied to virtual text
+    },
+    plugins = {
+        treesitter = true
+    },
+    options = {
+        italic = false,
+        cursorline = true
+    }
+}
+
+cmd [[ colorscheme onedarkpro ]]
+
+-- [[ Ensure packer ]]
 
 local function ensure_packer()
     local fn = vim.fn
@@ -32,6 +68,12 @@ local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
+    use 'olimorris/onedarkpro.nvim'
     use 'gruvbox-community/gruvbox'
 
     if packer_bootstrap then
